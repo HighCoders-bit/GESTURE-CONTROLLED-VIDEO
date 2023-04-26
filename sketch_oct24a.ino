e160d122ec21ac3249dce2cc2b8adfa2572647cd
HighCoders-bit <70643549+HighCoders-bit@users.noreply.github.com>
2023-04-26 17:28:47 +0530

    Update sketch_oct24a.ino

--- sketch_oct24a.ino
+++ sketch_oct24a.ino
@@ -1,47 +1,102 @@
 
-int trigPin = 11;    // Trigger
-int echoPin = 12;// Echo
+const int trigger1 = 2; //Trigger pin of 1st Sesnor
+const int echo1 = 3; //Echo pin of 1st Sesnor
+const int trigger2 = 11; //Trigger pin of 2nd Sesnor
+const int echo2 = 12;//Echo pin of 2nd Sesnor
 #define VCC2 8
+long time_taken;
+int dist,distL,distR;
 
-long duration, cm, inches;
- 
 void setup() {
-  //Serial Port begin
-  Serial.begin (9600);
-  //Define inputs and outputs
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(VCC2,HIGH);
+Serial.begin(9600); 
   
+pinMode(trigger1, OUTPUT); 
+pinMode(echo1, INPUT); 
+pinMode(trigger2, OUTPUT); 
+pinMode(echo2, INPUT);
+pinMode(VCC2, HIGH);
+digitalWrite(VCC2, HIGH);
+}           
+
+/*###Function to calculate distance###*/
+
+void calculate_distance(int trigger, int echo)
+{
+digitalWrite(trigger, LOW);
+delayMicroseconds(2);
+digitalWrite(trigger, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigger, LOW);
+
+time_taken = pulseIn(echo, HIGH);
+dist= time_taken*0.034/2;
+if (dist>60)
+dist = 60;
 }
- 
-void loop() {
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
- 
- 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
-  
- 
-  // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
-  
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  
-  delay(250);
+
+void loop() { 
+calculate_distance(trigger1,echo1);
+distL =dist; //get distance of left sensor
+
+calculate_distance(trigger2,echo2);
+distR =dist; //get distance of right sensor
+
+//Pause Modes -Hold
+if ((distL >40 && distR>40) && (distL <60 && distR<60)) //Detect both hands
+{
+Serial.println("Play/Pause"); 
+delay (500);
 }
+
+calculate_distance(trigger1,echo1);
+distL =dist;
+
+calculate_distance(trigger2,echo2);
+distR =dist;
+
+//Control Modes
+//Lock Left - Control Mode
+if (distL>=13 && distL<=17)
+{
+  delay(100); //Hand Hold Time
+  calculate_distance(trigger1,echo1);
+  distL =dist;
+  if (distL>=13 && distL<=17)
+  {
+    Serial.println("Left Locked");
+    while(distL<=40)
+    {
+      calculate_distance(trigger1,echo1);
+      distL =dist;
+      if (distL<10) //Hand pushed in 
+      {Serial.println ("Volume Increased"); delay (300);}
+      if (distL>20) //Hand pulled out
+      {Serial.println ("Volume Decreased"); delay (300);}
+    }
+  }
+}
+
+//Lock Right - Control Mode
+if (distR>=13 && distR<=17)
+{
+  delay(100); //Hand Hold Time
+  calculate_distance(trigger2,echo2);
+  distR =dist;
+  if (distR>=13 && distR<=17)
+  {
+    Serial.println("Right Locked");
+    while(distR<=40)
+    {
+      calculate_distance(trigger2,echo2);
+      distR =dist;
+      if (distR<10) //Right hand pushed in
+      {Serial.println ("Rewind"); delay (300);}
+      if (distR>20) //Right hand pulled out
+      {Serial.println ("Forward"); delay (300);}
+  }
+}
+}
+
+delay(200);
+}
+
